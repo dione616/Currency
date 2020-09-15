40ddf023b5989604e6eb5f8b65cbcce2fec8526e
dione616 <Vetal160199@i.ua>
2020-09-15 22:33:47 +0400

    Reverse done for first reverse, Need to fix it

--- src/components/Converter/Converter.tsx
+++ src/components/Converter/Converter.tsx
@@ -21,8 +21,6 @@ type PropsFromRedux = ConnectedProps<typeof connector>
 interface Props extends PropsFromRedux {}
 
 const Converter: React.FC<Props> = (props) => {
-  console.log(props)
-
   const { data } = props.data
   const [leftCurrency, setLeftCurrency] = useState<Currency>({
     count: 100,
@@ -33,6 +31,9 @@ const Converter: React.FC<Props> = (props) => {
     currency: "USD",
   })
   const [converted, setConverted] = useState(true)
+  const [reversed, setReversed] = useState(false)
+  const [reversed2, setReversed2] = useState({ done: false, firsTime: true })
+
   let convertedValue: number
 
   const mapedData = data.map((item) => {
@@ -42,37 +43,79 @@ const Converter: React.FC<Props> = (props) => {
   })
 
   const handleDropdown = (count: number, currency: string, side: string) => {
-    if (side === "left") {
-      setLeftCurrency({ count, currency })
+    if (!reversed2.done) {
+      if (side === "left") {
+        setLeftCurrency({ count, currency })
+      } else {
+        setRIghtCurrency({ count, currency })
+      }
     } else {
-      setRIghtCurrency({ count, currency })
+      if (side === "left") {
+        setRIghtCurrency({ count, currency })
+      } else {
+        setLeftCurrency({ count, currency })
+      }
     }
   }
 
   const handleConvert = () => {
-    const searchCurrency = mapedData.filter((note) => {
-      return note.base_ccy === leftCurrency.currency && note.ccy === rightCurrency.currency
-    })
+    if (!reversed2.done) {
+      const searchCurrency = mapedData.filter((note) => {
+        return note.base_ccy === leftCurrency.currency && note.ccy === rightCurrency.currency
+      })
 
-    if (searchCurrency.length > 0) {
-      //calculate
-      convertedValue = leftCurrency.count / parseFloat(searchCurrency[0].sale)
+      if (searchCurrency.length > 0) {
+        //calculate
+        convertedValue = leftCurrency.count / parseFloat(searchCurrency[0].sale)
 
-      return {
-        count: convertedValue,
-        currency: rightCurrency.currency,
+        return {
+          count: convertedValue,
+          currency: rightCurrency.currency,
+        }
+      } else {
+        setConverted(false)
+        return {
+          count: rightCurrency.count,
+          currency: rightCurrency.currency,
+        }
       }
     } else {
-      setConverted(false)
-      return {
-        count: rightCurrency.count,
-        currency: rightCurrency.currency,
+      const searchCurrency = mapedData.filter((note) => {
+        return note.ccy === leftCurrency.currency && note.base_ccy === rightCurrency.currency
+      })
+
+      if (searchCurrency.length > 0) {
+        //calculate
+        //onReverse Click just swap BUT then calculate
+        if (!reversed2.firsTime) {
+          convertedValue = rightCurrency.count
+          console.log(convertedValue)
+        } else {
+          convertedValue = rightCurrency.count / parseFloat(searchCurrency[0].sale)
+          console.log(convertedValue)
+        }
+        /* / parseFloat(searchCurrency[0].sale) */
+
+        return {
+          count: convertedValue,
+          currency: rightCurrency.currency,
+        }
+      } else {
+        setConverted(false)
+        return {
+          count: rightCurrency.count,
+          currency: rightCurrency.currency,
+        }
       }
     }
   }
 
   const handleReverse = () => {
-    console.log("reverse")
+    setReversed2({ done: !reversed2.done, firsTime: !reversed2.firsTime })
+    let l = leftCurrency
+    let r = rightCurrency
+    /*  setLeftCurrency(r)
+    setRIghtCurrency(l) */
   }
 
   const color = () => {
@@ -95,14 +138,32 @@ const Converter: React.FC<Props> = (props) => {
   }
 
   useEffect(() => {
-    if (handleConvert().count !== rightCurrency.count || handleConvert().currency !== rightCurrency.currency) {
-      setRIghtCurrency(handleConvert())
+    if (!reversed2.done) {
+      if (handleConvert().count !== rightCurrency.count || handleConvert().currency !== rightCurrency.currency) {
+        console.log("Effect !!!!reversed")
+        setRIghtCurrency(handleConvert())
+      }
+    } else {
+      console.log(rightCurrency.count, leftCurrency.count)
+      if (handleConvert().count !== rightCurrency.count || handleConvert().currency !== rightCurrency.currency) {
+        console.log(handleConvert().count, leftCurrency.count)
+
+        console.log("Effect reversed")
+        if (reversed2.firsTime) {
+          console.log("firstTime:", reversed2.firsTime)
+
+          setRIghtCurrency(rightCurrency)
+          setReversed2({ done: reversed2.done, firsTime: !reversed2.firsTime })
+        }
+      } else {
+        console.log("need convert", rightCurrency.count)
+      }
     }
 
     return () => {
       setConverted(true)
     }
-  }, [rightCurrency, leftCurrency])
+  }, [rightCurrency, leftCurrency, reversed2])
 
   return (
     <div className="wrapper">
@@ -112,10 +173,11 @@ const Converter: React.FC<Props> = (props) => {
           <div className="bottom">
             <Dropdown
               data={mapedData}
-              value={leftCurrency}
+              value={!reversed2.done ? leftCurrency : rightCurrency}
               placeholder="Select curr"
               onChange={handleDropdown}
               side={"left"}
+              reversed={reversed2.done}
             />
           </div>
         </div>
@@ -128,7 +190,13 @@ const Converter: React.FC<Props> = (props) => {
         <div className={`card right ${color()}`}>
           <h3>Get</h3>
           <div className="bottom">
-            <Dropdown data={mapedData} value={rightCurrency} onChange={handleDropdown} side={"right"} />
+            <Dropdown
+              data={mapedData}
+              value={!reversed2.done ? rightCurrency : leftCurrency}
+              onChange={handleDropdown}
+              side={"right"}
+              reversed={reversed2.done}
+            />
           </div>
         </div>
       </div>
--- src/components/Converter/Dropdown.tsx
+++ src/components/Converter/Dropdown.tsx
@@ -1,28 +1,9 @@
 import React, { useState, useEffect } from "react"
 
 const Dropdown = (props: any) => {
-  const { value, data, onChange, side } = props
-
+  const { value, data, onChange, side, reversed } = props
   const [currency, setCurrency] = useState(value.currency)
   const [count, setCount] = useState(value.count)
-
-  const handleCurrChange = (event: any, side: string) => {
-    console.log(event.target.tagName)
-
-    const currency = event.target.value
-
-    setCurrency(currency)
-
-    onChange(count, currency, side) //callback
-  }
-  const handlePriceChange = (event: any, side: string) => {
-    console.log(event.target.tagName)
-
-    const count = event.target.value
-    setCount(count)
-
-    onChange(count, currency, side) //callback
-  }
 
   const handleChange = (event: any, side: string) => {
     if (event.target.tagName == "SELECT") {
@@ -42,11 +23,11 @@ const Dropdown = (props: any) => {
       {side === "left" ? (
         <div>
           <input type="number" value={value.count} onChange={(e) => handleChange(e, "left")} />
-          <select value={value.currency} className="form-control" onChange={(e) => handleCurrChange(e, "left")}>
+          <select value={value.currency} className="form-control" onChange={(e) => handleChange(e, "left")}>
             {data.map((item: any, key: any) => {
               return (
-                <option key={key} value={item.base_ccy}>
-                  {item.base_ccy}
+                <option key={key} value={!reversed ? item.base_ccy : item.ccy}>
+                  {!reversed ? item.base_ccy : item.ccy}
                 </option>
               )
             })}
@@ -55,11 +36,11 @@ const Dropdown = (props: any) => {
       ) : (
         <div>
           <div className="rightResult">{value.count}</div>
-          <select value={value.currency} className="form-control" onChange={(e) => handleCurrChange(e, "rigth")}>
+          <select value={value.currency} className="form-control" onChange={(e) => handleChange(e, "rigth")}>
             {data.map((item: any, key: any) => {
               return (
-                <option key={key} value={item.ccy}>
-                  {item.ccy}
+                <option key={key} value={!reversed ? item.ccy : item.base_ccy}>
+                  {!reversed ? item.ccy : item.base_ccy}
                 </option>
               )
             })}
