55cbaf040b5087ba6925a6d1af30469486eb1653
dione616 <Vetal160199@i.ua>
2020-09-16 20:24:54 +0400

    Created RequestLimiter

--- src/components/Featured/Featured.tsx
+++ src/components/Featured/Featured.tsx
@@ -4,6 +4,7 @@ import CurrencyList from "./CurrencyList"
 
 import { selectCurrencyData, loadCurrencyData } from "../../redux/currency"
 import { RootState } from "../../redux/store"
+import Converter from "./Converter"
 
 const mapStateToProps = (state: RootState) => {
   return {
@@ -18,16 +19,43 @@ type PropsFromRedux = ConnectedProps<typeof connector>
 interface Props extends PropsFromRedux {}
 
 const Featured: React.FC<Props> = (props) => {
+  const [show, setShow] = useState(true)
   const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(loadCurrencyData())
+    const count = localStorage.getItem("count")
+    if (count) {
+      let increase = parseInt(count)
+      if (increase % 5 !== 0 && increase !== 0) {
+        dispatch(loadCurrencyData())
+        increase += 1
+        const valToSend = String(increase)
+        localStorage.setItem("count", valToSend)
+      } else {
+        setShow(false)
+        let increase = parseInt(count)
+        increase += 1
+        const valToSend = String(increase)
+        localStorage.removeItem("count")
+      }
+    } else {
+      dispatch(loadCurrencyData())
+      localStorage.setItem("count", "2")
+    }
+
     return () => {}
   }, [])
 
   return (
     <div>
-      <CurrencyList prop={props.data} />
+      {show ? (
+        <div>
+          <CurrencyList prop={props.data} /> <h2>Exchenge</h2>
+          <Converter />
+        </div>
+      ) : (
+        <div className="error">To many requests</div>
+      )}
     </div>
   )
 }
